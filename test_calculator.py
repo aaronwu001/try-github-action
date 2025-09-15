72008fd64af340b11b851bf3172f293c3b3d8054
Aaron <aaronwu000@gmail.com>
2025-09-15 19:57:10 -0400

    basic things

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+from calculator import add, heheheha
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -2) == -1
+
+def test_heheheha():
+    assert heheheha("hello?") == "heheheheheheha"
+
